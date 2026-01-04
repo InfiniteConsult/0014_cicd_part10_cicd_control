53fc805ece11c9b7d8e1621f060d55b356152d03
Warren Jitsing <warren.jitsing@infcon.co.za>
2026-01-04 21:12:32 +0200

    :white_check_mark: test(UrllibTransport): test happy and http error paths

--- tests/test_urllib_transport.py
+++ tests/test_urllib_transport.py
@@ -1,7 +1,117 @@
-from cicd_control.urllib_transport import UrllibTransport
+import ssl
 
+
+from typing import Any
+
+
+import pytest
+
+
+from cicd_control.transport import HttpResponse
+from cicd_control.urllib_transport import UrllibTransport, HttpResponse
+
+
+from conftest import MockServer
+
+
+def get_mock_server_url(server: MockServer) -> str:
+    addr: str
+    port: int
+    addr, port = server.get_server_address()
+    url: str = f"https://{addr}:{port}"
+    return url
+
+
+def set_single_response_callback(server: MockServer, server_response: bytes) -> None:
+    def callback(callback_server: MockServer, sock: ssl.SSLSocket):
+        try:
+            _: bytes = callback_server.read_full_message(sock)
+            sock.sendall(server_response)
+        except Exception as exc:
+            print(exc)
+    server.set_server_callback(callback)
+
+
+def get_response(
+        ctx: ssl.SSLContext,
+        method: str,
+        url: str,
+        headers: dict[str, str] | None = None,
+        body: bytes | None = None
+) -> HttpResponse | None:
+    transport: UrllibTransport = UrllibTransport(ctx)
+    response: HttpResponse = transport.request(method, url, headers, body)
+    return response
+
+
+expected_and_http_error_paths: list[tuple[bytes, bytes, bytes, int, bytes | dict[str, str]]] = [
+    (
+        b"HTTP/1.1 200 OK",
+        b"Content-Type: application/json\r\nContent-Length: 18",
+        b'{"status": "json"}',
+        200,
+        {"status": "json"}
+    ),
+    # Case 2: Happy Path Text
+    (
+        b"HTTP/1.1 200 OK",
+        b"Content-Type: text/plain\r\nContent-Length: 10",
+        b"Just bytes",
+        200,
+        b"Just bytes"
+    ),
+    # Case 3: 404 Not Found (Handled by except HTTPError branch)
+    (
+        b"HTTP/1.1 404 Not Found",
+        b"Content-Type: application/json\r\nContent-Length: 15",
+        b'{"err": "oops"}',
+        404,
+        {"err": "oops"}
+    ),
+    # Case 4: 500 Server Error
+    (
+        b"HTTP/1.1 500 Server Error",
+        b"Content-Type: text/plain\r\nContent-Length: 12",
+        b"Server crash",
+        500,
+        b"Server crash"
+    )
+]
 
 class TestUrllibTransport:
-    def test_init(self, mock_server):
-        print(mock_server)
-        print(mock_server.ssl_ctx)
+    def test_init(self, client_context):
+        transport = UrllibTransport()
+        assert transport.context is not None
+        assert transport.context != client_context
+
+        transport = UrllibTransport(client_context)
+        assert transport.context is not None
+        assert transport.context == client_context
+
+    @pytest.mark.parametrize(
+        "status_line, headers, body_bytes, expected_status, expected_body",
+        expected_and_http_error_paths
+    )
+    def test_http_responses(
+            self,
+            mock_server,
+            client_context,
+            status_line,
+            headers,
+            body_bytes,
+            expected_status,
+            expected_body
+    ):
+        full_response = status_line + b"\r\n" + headers + b"\r\n\r\n" + body_bytes
+
+        set_single_response_callback(mock_server, full_response)
+
+        url = get_mock_server_url(mock_server)
+        transport = UrllibTransport(client_context)
+        response = transport.request("GET", url)
+        cleaned_headers: dict[str, str] = {
+            k.decode(): v.decode().strip() for k, v in [x.split(b":") for x in headers.split(b"\r\n")]
+        }
+        assert response.status_code == expected_status
+        assert response.body == expected_body
+        assert response.headers == cleaned_headers
